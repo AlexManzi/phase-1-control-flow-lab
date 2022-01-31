e4c83a7627febbf25b9133f5357dacb7dbb9a433
Alex Manzi <alex.manzi22@gmail.com>
2022-01-31 12:39:50 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  if (someNumber <= 400) {
+    return "This one is on me!"
+  } if (someNumber >= 2000 && someNumber <2500) {
+    return "I will gladly take your thirty bucks."
+  } else if (someNumber >= 2500) {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let response = (city === "NYC") ? "Ok, sounds good." : "No go."
+  return response
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case "generous":
+      return "Thank you so much." 
+      break;
+    case "not as generous":
+      return "Thank you." 
+      break;
+    default:
+      return "Bye." 
+  }
 }
