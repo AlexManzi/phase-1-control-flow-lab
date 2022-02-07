2c781305403061715f439f1566d6e1ef0c55f6c9
Alex Manzi <alex.manzi22@gmail.com>
2022-02-07 13:32:06 -0500

    commit tree 582f1c0e9fc370ac5ab7af0aff4304d509076796

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
-function scuberGreetingForFeet(someNumber){
-  if (someNumber <= 400) {
-    return "This one is on me!"
-  } if (someNumber >= 2000 && someNumber <2500) {
-    return "I will gladly take your thirty bucks."
-  } else if (someNumber >= 2500) {
-    return "No can do."
+function scuberGreetingForFeet(ride) {
+  if (ride <= 400) {
+    return 'This one is on me!'
+  }
+  if (ride >= 2000 && ride <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  if (ride > 2500) {
+    return 'No can do.'
   }
 }
 
-function ternaryCheckCity(city){
-  let response = (city === "NYC") ? "Ok, sounds good." : "No go."
-  return response
+function ternaryCheckCity(city) {
+  if (city === "NYC") {
+    return "Ok, sounds good."
+  }
+  if (city !== "NYC") {
+    return "No go." 
+  }
 }
 
-function switchOnCharmFromTip(tip){
-  switch (tip) {
-    case "generous":
-      return "Thank you so much." 
-      break;
-    case "not as generous":
-      return "Thank you." 
-      break;
-    default:
-      return "Bye." 
+function switchOnCharmFromTip(tip) {
+  if (tip === 'generous') {
+    return "Thank you so much." 
+  }
+  if (tip === 'not as generous') {
+    return "Thank you."
+  }
+  if (tip === 'thanks for everything') {
+    return "Bye."  
   }
 }
